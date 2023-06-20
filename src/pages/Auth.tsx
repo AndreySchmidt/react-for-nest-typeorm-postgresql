566440f8dc2cb8de922753b5aca91e7680617a72
AndreySchmidt <ringnull@gmail.com>
2023-06-20 14:19:00 +0300

    Auth

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,52 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 const Auth: FC = () => {
-  return "<div></div>";
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  return (
+    <div className="mt-40 flex flex-col items-center justify-center bg-slate-900 text-white">
+      <h1 className="mb-10 text-center text-xl">
+        {isLogin ? "Login" : "Registration"}
+      </h1>
+      <form className="gat-5 mx-auto flex w-1/3 flex-col">
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="text"
+          className="input"
+          placeholder="Email"
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="input"
+          placeholder="Password"
+        />
+        <button className="btn btn-green mx-auto">Submit</button>
+      </form>
+
+      <div className="mt-5 flex justify-center">
+        {isLogin ? (
+          <button
+            onClick={() => setIsLogin(!isLogin)}
+            className="text-slate-300 hover:text-white"
+          >
+            Account -
+          </button>
+        ) : (
+          <button
+            onClick={() => setIsLogin(!isLogin)}
+            className="text-slate-300 hover:text-white"
+          >
+            Account +
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
