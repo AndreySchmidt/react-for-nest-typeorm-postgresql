8400fa81d3520af8a268ea3068485fde3da9d6ce
AndreySchmidt <ringnull@gmail.com>
2023-06-21 15:07:38 +0300

    axios + token helpers

---
+++ src/api/axios.api.ts
@@ -0,0 +1,11 @@
+// npm i axios
+
+import axios from "axios";
+import { getTokenFromLocalStorage } from "../helpers/localstorage.helper";
+
+export const instance = axios.create({
+  baseURL: "http://localhost:3001/api",
+  headers: {
+    Autorization: `Bearer` + getTokenFromLocalStorage() || "",
+  },
+});
